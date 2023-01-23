d37366958a20d24ee207dbadf3853355dcb66170
“your <londal.anette@gmail.com>
2023-01-23 12:10:17 +0100

    AL: Initial commit.

---
+++ src/main/java/com/example/assignmentwebapi/AssignmentWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignmentwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentWebApiApplication.class, args);
+    }
+
+}
