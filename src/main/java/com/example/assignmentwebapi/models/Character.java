1a247cb5cb1f5c6314dc11d7a60744410df19cb9
“your <londal.anette@gmail.com>
2023-01-24 14:32:53 +0100

    AL: Fixed customer entety

--- src/main/java/com/example/assignmentwebapi/models/Character.java
+++ src/main/java/com/example/assignmentwebapi/models/Character.java
@@ -6,19 +6,79 @@ import org.springframework.stereotype.Component;
 
 @Entity
 public class Character {
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long characterId;
 
+    public String getForName() {
+        return forName;
+    }
+
+    public void setForName(String forName) {
+        this.forName = forName;
+    }
+
     @Column(nullable = false, length = 30)
     private String forName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     @Column(nullable = false, length = 40)
     private String lastName;
 
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    @Column(nullable = false, length = 40)
+    private String alias;
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Gender gender;
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    @Column()
+    private String pictureUrl;
+
+
+    public long getCharacterId() {
+        return characterId;
+    }
+
+    public void setCharacterId(long characterId) {
+        this.characterId = characterId;
+    }
+
+
+
 
 
 }
