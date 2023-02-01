233be5c8c2cf8d8c58a7fe6339c47ac72e3c1357
HessianThespian <adrianfriduson@gmail.com>
2023-02-01 13:40:07 +0100

    Controller: Changed typo in CharacterController.

--- src/main/java/com/example/assignmentwebapi/controllers/CharacterController.java
+++ src/main/java/com/example/assignmentwebapi/controllers/CharacterController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/api/vi/character")
+@RequestMapping(path = "/api/v1/character")
 public class CharacterController {
 
 
