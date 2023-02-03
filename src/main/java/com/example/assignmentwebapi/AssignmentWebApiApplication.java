a336c531f64c1173555bc90b0dd4136695d85c89
HessianThespian <adrianfriduson@gmail.com>
2023-02-03 09:54:19 +0100

    App: Added a description, title and version number in AssignmentWebApiApplication, and removed an unused import.

--- src/main/java/com/example/assignmentwebapi/AssignmentWebApiApplication.java
+++ src/main/java/com/example/assignmentwebapi/AssignmentWebApiApplication.java
@@ -1,10 +1,16 @@
 package com.example.assignmentwebapi;
 
-import org.springframework.boot.ApplicationArguments;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "Movie Characters API",
+        description = "A REST API to provide access to movies, the characters that are in those movies, and the franchises those movies belong to.",
+        version = "1.0"
+))
 public class AssignmentWebApiApplication {
 
     public static void main(String[] args) {
